91d6b0f907d89cfb74f7f82ddcc49d492120224a
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-04-09 16:33:15 -0700

    conflicts cleaned

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 'use strict';
 
-/*console.log(
-  (document.querySelector('.message').textContent = 'Correct Number!')
-);
-
-console.log((document.querySelector('.number').textContent = 7));
-document.querySelector('.guess').value = 15;
-*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -46,25 +39,6 @@ document.querySelector('.check').addEventListener('click', function () {
     displayMessage('You lost the game!');
     document.querySelector('.score').textContent = 0;
   }
-  // } else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'Too high';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = 'You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = 'Too low!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = 'You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
